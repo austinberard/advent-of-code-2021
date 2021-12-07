b35c02f27cc49fde9c10d9a30ced53a4c4b3034d
Austin Berard <austinberard@outlook.com>
2021-12-06 19:14:18 -0500

    Complete part 2
    Switch to storing to number of fish with the index.
    This is much faster.

--- src/6.py
+++ src/6.py
@@ -1,22 +1,30 @@
 from pprint import pprint
 
 if __name__ == '__main__':
-  days = 80
-  fish = None
+  days = 256
+  fish = [0] * 10
   with open("../Data/day-6.txt", 'r') as f:
     lines = f.readlines()
-    fish = [int(x) for x in lines[0].strip().split(',')]
+    fish_to_add = [int(x) for x in lines[0].strip().split(',')]
+
+    for item in fish_to_add:
+      fish[item] += 1
+
 
   while days > 0:
-    new_fish = []
-    for idx, f in enumerate(fish):
-      if f == 0:
-        fish[idx] = 6
-        new_fish.append(8)
+    new_fish_list = fish # is this a copy? or an alias
+    for idx, num_of_fish in enumerate(fish):
+      if num_of_fish == 0:
+        continue
+      if idx == 0:
+        new_fish_list[9] += num_of_fish
+        new_fish_list[0] -= num_of_fish
+        new_fish_list[7] += num_of_fish
       else:
-        fish[idx] = f - 1
-    fish += new_fish
+        new_fish_list[idx] -= num_of_fish
+        new_fish_list[idx-1] += num_of_fish
+    fish = new_fish_list
     days -= 1
 
-  print(len(fish)) 
+  print(sum(fish))
   
